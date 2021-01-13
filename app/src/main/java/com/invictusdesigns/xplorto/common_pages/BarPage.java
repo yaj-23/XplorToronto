9d41d36c935c4dd5dd203bbc51c1b181ba3a10db
yaj-23 <yajurva.trivedi@ryerson.ca>
2021-01-13 06:47:11 -0500

    Fixed Bar-page, removed copy

--- app/src/main/java/com/invictusdesigns/xplorto/common_pages/BarPage.java
+++ app/src/main/java/com/invictusdesigns/xplorto/common_pages/BarPage.java
@@ -29,15 +29,12 @@ public class BarPage extends AppCompatActivity {
         buisness_img2 = findViewById(R.id.bar_buisness_img2);
         buisness_img3 = findViewById(R.id.bar_buisness_img3);
         buisness_img4 = findViewById(R.id.bar_buisness_img4);
-        buisness_img5 = findViewById(R.id.bar_buisness_img5);
-        buisness_img6 = findViewById(R.id.bar_buisness_img6);
+
 
         buisness_title = findViewById(R.id.bar_buisness_title);
         buisness_title2 = findViewById(R.id.bar_buisness_title2);
         buisness_title3 = findViewById(R.id.bar_buisness_title3);
         buisness_title4 = findViewById(R.id.bar_buisness_title4);
-        buisness_title5 = findViewById(R.id.bar_buisness_title5);
-        buisness_title6 = findViewById(R.id.bar_buisness_title6);
 
         // Bar Raval  CLICK LISTENER
         buisness_img.setOnClickListener(new View.OnClickListener() {
@@ -155,64 +152,6 @@ public class BarPage extends AppCompatActivity {
             }
         });
 
-        /**
-        // Summer's CLICK LISTENER
-        buisness_img5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String summers = "https://www.summersicecream.com/";
-                Uri webaddress = Uri.parse(summers);
-
-                Intent intent = new Intent(Intent.ACTION_VIEW, webaddress);
-                if (intent.resolveActivity(getPackageManager()) != null){
-                    startActivity(intent);
-                }
-            }
-        });
-
-        buisness_title5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String summers = "https://www.summersicecream.com/";
-                Uri webaddress = Uri.parse(summers);
-
-                Intent intent = new Intent(Intent.ACTION_VIEW, webaddress);
-                if (intent.resolveActivity(getPackageManager()) != null){
-                    startActivity(intent);
-                }
-
-            }
-        });
-
-        // Booyah CLICK LISTENER
-        buisness_img6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String booyah = "https://booyah-inc.com/";
-                Uri webaddress = Uri.parse(booyah);
-
-                Intent intent = new Intent(Intent.ACTION_VIEW, webaddress);
-                if (intent.resolveActivity(getPackageManager()) != null){
-                    startActivity(intent);
-                }
-            }
-        });
-
-        buisness_title6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String booyah = "https://booyah-inc.com/";
-                Uri webaddress = Uri.parse(booyah);
-
-                Intent intent = new Intent(Intent.ACTION_VIEW, webaddress);
-                if (intent.resolveActivity(getPackageManager()) != null){
-                    startActivity(intent);
-                }
-
-            }
-        });*/
 
     }
 }
