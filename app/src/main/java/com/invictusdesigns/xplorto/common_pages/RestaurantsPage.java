e3e17abf7cf8fa7030c231ffad8dc1d883bd077c
yaj-23 <yajurva.trivedi@ryerson.ca>
2021-01-13 23:59:50 -0500

    progress

--- app/src/main/java/com/invictusdesigns/xplorto/common_pages/RestaurantsPage.java
+++ app/src/main/java/com/invictusdesigns/xplorto/common_pages/RestaurantsPage.java
@@ -1,13 +1,19 @@
 package com.invictusdesigns.xplorto.common_pages;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.invictusdesigns.xplorto.R;
+import com.invictusdesigns.xplorto.restaurant_types.Indian;
 
 public class RestaurantsPage extends AppCompatActivity {
+
+    CardView firstcard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,15 @@ public class RestaurantsPage extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_restaurants_page);
+
+        firstcard = findViewById(R.id.firstcard);
+
+        firstcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Indian.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/invictusdesigns/xplorto/restaurant_types/Indian.java
+++ app/src/main/java/com/invictusdesigns/xplorto/restaurant_types/Indian.java
@@ -2,14 +2,21 @@ package com.invictusdesigns.xplorto.restaurant_types;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.invictusdesigns.xplorto.R;
 
 public class Indian extends AppCompatActivity {
-
+    ImageView to_dashboard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indian);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+
     }
 }
