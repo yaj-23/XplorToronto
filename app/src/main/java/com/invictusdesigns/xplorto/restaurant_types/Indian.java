d8dd59a5e3a020e033d18c514951d763d505440b
yaj-23 <yajurva.trivedi@ryerson.ca>
2021-01-14 23:46:28 -0500

    progress2.0

--- app/src/main/java/com/invictusdesigns/xplorto/restaurant_types/Indian.java
+++ app/src/main/java/com/invictusdesigns/xplorto/restaurant_types/Indian.java
@@ -1,11 +1,15 @@
 package com.invictusdesigns.xplorto.restaurant_types;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.invictusdesigns.xplorto.R;
+import com.invictusdesigns.xplorto.common_pages.UserDashBoard;
 
 public class Indian extends AppCompatActivity {
     ImageView to_dashboard;
@@ -15,6 +19,15 @@ public class Indian extends AppCompatActivity {
         setContentView(R.layout.activity_indian);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        to_dashboard=findViewById(R.id.to_dasboard);
+
+        to_dashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UserDashBoard.class);
+                startActivity(intent);
+            }
+        });
 
 
 
