bed2c7ff1ae752f134e7353121d7e9c0246a0f39
yaj-23 <yajurva.trivedi@ryerson.ca>
2021-01-30 00:20:37 -0500

    Carribean Page Setup Completed

--- app/src/main/java/com/invictusdesigns/xplorto/common_pages/RestaurantsPage.java
+++ app/src/main/java/com/invictusdesigns/xplorto/common_pages/RestaurantsPage.java
@@ -9,11 +9,12 @@ import android.view.View;
 import android.view.WindowManager;
 
 import com.invictusdesigns.xplorto.R;
+import com.invictusdesigns.xplorto.restaurant_types.Caribbean;
 import com.invictusdesigns.xplorto.restaurant_types.Indian;
 
 public class RestaurantsPage extends AppCompatActivity {
 
-    CardView firstcard;
+    CardView firstcard, thirdcard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +24,20 @@ public class RestaurantsPage extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants_page);
 
         firstcard = findViewById(R.id.firstcard);
+        thirdcard = findViewById(R.id.thirdcard);
 
         firstcard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Indian.class);
+                startActivity(intent);
+            }
+        });
+
+        thirdcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Caribbean.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/invictusdesigns/xplorto/restaurant_types/Caribbean.java
+++ app/src/main/java/com/invictusdesigns/xplorto/restaurant_types/Caribbean.java
@@ -2,15 +2,42 @@ package com.invictusdesigns.xplorto.restaurant_types;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.invictusdesigns.xplorto.R;
+import com.invictusdesigns.xplorto.common_pages.UserDashBoard;
 
 public class Caribbean extends AppCompatActivity {
+    ImageView to_dashboard,caribbean_buisness_img,caribbean_buisness_img2,caribbean_buisness_img3,caribbean_buisness_img4,caribbean_buisness_img5,caribbean_buisness_img6,caribbean_buisness_img7,caribbean_buisness_img8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caribbean);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        to_dashboard=findViewById(R.id.to_dasboard);
+        caribbean_buisness_img=findViewById(R.id.caribbean_buisness_img);
+        caribbean_buisness_img2=findViewById(R.id.caribbean_buisness_img2);
+        caribbean_buisness_img3=findViewById(R.id.caribbean_buisness_img3);
+        caribbean_buisness_img4=findViewById(R.id.caribbean_buisness_img4);
+        caribbean_buisness_img5=findViewById(R.id.caribbean_buisness_img5);
+        caribbean_buisness_img6=findViewById(R.id.caribbean_buisness_img6);
+        caribbean_buisness_img7=findViewById(R.id.caribbean_buisness_img7);
+        caribbean_buisness_img8=findViewById(R.id.caribbean_buisness_img8);
+
+
+        to_dashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UserDashBoard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
